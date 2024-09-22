5c481d89832e2812ac22208e49fe326120753183
VovaUshakov <vova06002@yandex.ru>
2024-09-23 02:34:48 +0700

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

---
+++ 1dz.py
@@ -0,0 +1,23 @@
+# 1st program
+
+print((9**0.5)*5)
+
+# 2st program
+
+print(9.99>9.98 and 1000!= 1000.1)
+
+# 3st program
+
+x=2*2+2
+y=2*(2+2)
+print(x)
+print(y)
+print(x==y)
+
+# 4st program
+
+p='123.456'
+p= float(p)
+k=int(p)
+l=p-k
+print(int(l*10))
